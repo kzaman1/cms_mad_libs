33d6cf48112ed8a55ea54a093bda74e457b82de9
Khuram Zaman <khuram.zaman1@gmail.com>
2020-10-29 00:09:19 -0400

    Initial commit

---
+++ clue_mad_libs.py
@@ -0,0 +1,5 @@
+character = input("Pick a clue character: ")
+cake = input("Pick a type of cake: ")
+drink = input("Pick a type of drink: ")
+
+print("It was a dark and stormy night when they discovered that", character, "had eaten the last slice of", cake, "and had even finished all of the", drink, "DUN DUN DUNNNN!!!")
